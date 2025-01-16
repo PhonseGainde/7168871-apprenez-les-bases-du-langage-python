05da464d35f6e854351283f10e8d8cdd0f9224d5
PhonseGainde <alphonsendiaye@hotmail.fr>
2025-01-16 17:30:31 +0100

    Update main.py

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,6 @@
-# Ecrivez votre code ici !
+nombre1 = input("veuillez entrer un premier nombre!)
+nombre2 = input("veuillez entrer un deuxieme nombre!)
+
+resultat = 0
+
+if (nombre1, nombre2) isnumeric
